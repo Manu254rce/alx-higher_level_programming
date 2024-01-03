4f1e77a61176a689361eaa9da1197027770f218c
Manu254rce <straussmoabit@gmail.com>
2024-01-03 12:49:51 -0800

    Finished advanced tasks

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    str2 = ""
+    for  i in range(len(str)):
+        if (i == n):
+           continue
+        else:
+            str2 += str[i]
+    print("{}".format(str2))
