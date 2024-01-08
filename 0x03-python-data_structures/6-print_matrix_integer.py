15ed079fde2dc7bb4f36c9e08fd951b602e4c114
Manu254rce <straussmoabit@gmail.com>
2024-01-09 01:37:25 +0300

    Completed task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            print("{:d}".format(matrix[i][j]), end=" ")
-        print("$")
+    for i in matrix:
+        for j in i:
+            print("{:d}".format(j), end="$")
+        print()
 
 if __name__ == "__main__":
     print_matrix_integer(matrix)
