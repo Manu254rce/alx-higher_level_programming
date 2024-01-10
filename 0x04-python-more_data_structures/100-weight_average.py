1bc083b32f74498cd24e12d6f712492b8cce9737
Manu254rce <straussmoabit@gmail.com>
2024-01-11 01:09:29 +0300

    Completed task 100

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    return 0 if not my_list
+    if not my_list:
+        return 0
 
     prod = 0
     add = 0
     for score, weight in my_list:
         add += weight
         prod += (score * weight)
-
-    raise ValueError("Total weight can't be zero") if add == 0
+    if add == 0:
+        raise ValueError("Total weight can't be zero")
 
     return prod / add
