ca9e077d349e58b1bb6c952bec194249b0b8d4da
Manu254rce <straussmoabit@gmail.com>
2024-01-10 20:43:10 +0300

    Completed task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -6,3 +6,7 @@ def square_matrix_simple(matrix=[]):
         for j in range(len(matrix[0])):
             sq_matrix[i][j] = matrix[i][j] ** 2
     return(sq_matrix)
+
+
+if __name__ == "__main__":
+    square_matrix_simple(matrix)
