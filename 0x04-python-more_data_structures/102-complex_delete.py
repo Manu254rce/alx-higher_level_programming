ac9bbc8b8d33b30657c7e0bfdde9cef0c865709b
Manu254rce <straussmoabit@gmail.com>
2024-01-11 01:30:58 +0300

    Completed task 102

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    if not a_dictionary:
+        return a_dictionary
+    else:
+        for key in list(a_dictionary):
+            if a_dictionary[key] == value:
+                del a_dictionary[key]
+
+        return a_dictionary
