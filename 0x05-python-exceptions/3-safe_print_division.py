f6961a9dd3ffe224e3ee9b57f9e7d89b583c5f0b
Manu254rce <straussmoabit@gmail.com>
2024-01-23 01:10:17 +0300

    Fixed some errors

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -13,14 +13,3 @@ def safe_print_division(a, b):
         else:
             print("Inside result: None")
     return result
-
-if __name__ == "__main__":
-    a = 12
-    b = 2
-    result = safe_print_division(a, b)
-    print("{:d} / {:d} = {}".format(a, b, result))
-
-    a = 12
-    b = 0
-    result = safe_print_division(a, b)
-    print("{:d} / {:d} = {}".format(a, b, result))
