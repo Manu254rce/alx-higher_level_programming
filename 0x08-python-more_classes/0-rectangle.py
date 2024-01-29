6ccf79a206d02d2a81592dac59764c9ddeb2bc85
Manu254rce <straussmoabit@gmail.com>
2024-01-29 16:38:17 +0300

    Completed task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This is a Python class definition
+"""
+
+
+class Rectangle:
+    """
+    This Python class defines an
+    empty class for a rectangle figure
+    """
+    pass
