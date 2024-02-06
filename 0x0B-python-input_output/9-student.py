47825f2a53c29489a1896cebd6d0c541822332ee
Manu254rce <straussmoabit@gmail.com>
2024-02-06 23:27:50 +0300

    Completed task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -5,12 +5,24 @@ This is Python3 code
 
 
 class Student:
+    """
+    This cclass defines functions that define and
+    return dictinary list for Student attributes
+    """
     def __init__(self, first_name, last_name, age):
+        """
+        Function that is a class constructor
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
+        """
+        Function that returns dictionary structure for
+        student attributes
+        """
         if attrs is None:
             return self.__dict__
-        return {attr: getattr(self, attr) for attr in attrs if attr in self.__dict__}
+        return {attr: getattr(self, attr) for attr in attrs
+                if attr in self.__dict__}
