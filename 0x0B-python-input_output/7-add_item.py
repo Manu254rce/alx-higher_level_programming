2def00d76d45cb94b7a0b75004e2c8405e8ac32f
Manu254rce <straussmoabit@gmail.com>
2024-02-06 21:13:03 +0300

    Completed task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This is a Python3 code
+"""
+
+
+from sys import argv
+from importlib import import_module
+
+
+file = 'add_item.json'
+
+save_json = import_module('5-save_to_json_file').save_to_json_file
+load_json = import_module('6-load_from_json_file').load_from_json_file
+
+try:
+    obj = load_json(file)
+except FileNotFoundError:
+    obj = []
+
+obj.extend(argv[1:])
+save_json(obj, file)
