ed1c8c90762f7d695bb9d46e381f5ce457af6c81
Manu254rce <straussmoabit@gmail.com>
2024-02-06 14:42:50 +0300

    Completed task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This is Python 3 code
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+    This function returns the JSON rpresentation
+    of a string
+    """
+    return json.dumps(my_obj)
