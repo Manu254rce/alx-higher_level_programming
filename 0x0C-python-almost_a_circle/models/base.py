6b38fd6f7e2ae5bd5f8b89332dfbc19475bfd56e
Manu254rce <straussmoabit@gmail.com>
2024-02-11 23:07:12 +0300

    Working on tasks 0 and 1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This is Python3 code
+"""
+
+
+class Base:
+    """
+    This is a Python class which is the 'base' class in this case
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Function that initializes attributes
+        """
+        if id is not None:
+            self.__id = int(id)
+
+        Base.__nb_objects += 1
