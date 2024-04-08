d7b7a347a4d0d3ea852b0e9f6ab8d5becc687dc0
Manu254rce <straussmoabit@gmail.com>
2024-04-08 23:09:26 +0300

    Task 100 done

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-function executeTimes(x, func) {
-  let i = 0;
-  for(i = 0; i < x; i++) {
-    func()
-  }
+export function executeTimes (x, func) {     
+  for (let i = 0; i < x; i++) {
+    func();
+  } 
 }
