e5bbed7790b55696ea1d6100db29d9d8638d2df9
Manu254rce <straussmoabit@gmail.com>
2024-04-08 22:47:55 +0300

    Task 100 done

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const arg1 = process.argv[2]
-const arg2 = process.argv[3]
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
 if (process.argv.length <= 3) {
-  console.log('No argument')
+  console.log('No argument');
 } else {
-  console.log(arg1 + ' is ' + arg2)
+  console.log(arg1 + ' is ' + arg2);
 }
