e2f2482eb252f56461aa11ff9d6781cfe204432e
Manu254rce <straussmoabit@gmail.com>
2024-04-26 00:51:08 +0300

    Task 0 done

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes in a URL, sends a request to that URL displaying the size
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}' | tr -d '\r'
+curl -sI "$1" | grep "Content-Length" | cut -d' ' -f2
