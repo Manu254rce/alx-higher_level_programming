e80b11fb636c5c183829d869e2c57acd3bab407e
Manu Lamo <straussmoabit@gmail.com>
2024-05-15 21:11:09 +0300

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/4-concat.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-const arg1 = process.argv[2];
-const arg2 = process.argv[3];
-if (process.argv.length <= 3) {
-  console.log('No argument');
-} else {
-  console.log(arg1 + ' is ' + arg2);
-}
